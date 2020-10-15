7689a0b0fbd5cfb6f9b2781e0972d2a6ac2ef306
pjachimowski <p.jachimowski89@gmail.com>
2020-10-15 10:05:21 +0200

    contact page first draft

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Contact extends Component {
-    render(){
-        return(
-            <div><h1>Contact Page</h1></div>
+    render() {
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>half a project</Cell>
+                    <Cell col={6}>half a project</Cell>
+                </Grid>
+            </div>
         );
     }
 }
