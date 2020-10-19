2b9b23b8d3a4bebc601825e2998755120a40e47d
pjachimowski <p.jachimowski89@gmail.com>
2020-10-19 23:53:21 +0200

    landing page first styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/Main';
-import './App.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterDropDownSection, FooterLinkList } from "react-mdl";
+import Main from "./components/Main";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+// style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}
 
 class App extends Component {
   render() {
     return (
-      <div className="demo-big-content">
+      <div>
         <Layout>
-          <Header className="header-color" title="Title" scroll>
+          <Header
+            className="nav-translusent"
+            transparent
+            title={
+              <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                Home
+              </Link>
+            }
+            scroll
+          >
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
@@ -25,10 +36,49 @@ class App extends Component {
               <Link to="/contact">Contact</Link>
             </Navigation>
           </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
+          <Content />
+          <Main />
+          <Footer className="footer" size="mega">
+            <FooterSection type="middle">
+              <FooterDropDownSection title="Features">
+                <FooterLinkList>
+                  <a href="#">About</a>
+                  <a href="#">Terms</a>
+                  <a href="#">Partners</a>
+                  <a href="#">Updates</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="Details">
+                <FooterLinkList>
+                  <a href="#">Specs</a>
+                  <a href="#">Tools</a>
+                  <a href="#">Resources</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="Technology">
+                <FooterLinkList>
+                  <a href="#">How it works</a>
+                  <a href="#">Patterns</a>
+                  <a href="#">Usage</a>
+                  <a href="#">Products</a>
+                  <a href="#">Contracts</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="FAQ">
+                <FooterLinkList>
+                  <a href="#">Questions</a>
+                  <a href="#">Answers</a>
+                  <a href="#">Contact Us</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+            </FooterSection>
+            <FooterSection type="bottom" logo="Title">
+              <FooterLinkList>
+                <a href="#">Help</a>
+                <a href="#">Privacy & Terms</a>
+              </FooterLinkList>
+            </FooterSection>
+          </Footer>
         </Layout>
       </div>
     );
--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -4,25 +4,28 @@ import { Grid, Cell } from 'react-mdl';
 class LandingPage extends Component {
     render() {
         return (
-            <div style={{ width: '100%', margin: 'auto' }}>
+            <div className="bg">
                 <Grid className="landing-grid">
                     <Cell col={12}>
+                        <h1>Full Stack Web Developer</h1>
                         <img
                             src="https://i.ibb.co/v1VDC5M/PATRYK-JACHIMOWSKI-2020.jpg"
                             alt="avatar"
                             className="avatar-img"
                         />
-                        <div className="banner-text">
-                            <h1>Full Stack Web Developer</h1>
+                        <h2>Patryk Jachimowski</h2>
+                        
+
+
                             <hr />
-                            <p>JavaScript | Css | Html | React | Express | NodeJS | MongoDB</p>
+                            <p>JavaScript | CSS | HTML | React | Express | NodeJS | MongoDB</p>
             `               <div className="social-links">
                                 <a href="https://www.facebook.com/patryk.jachimowski.73/" rel="noopener noreferrer" target="_blank"><i class="fab fa-facebook"></i></a>
                                 <a href="https://github.com/pjachimowski" rel="noopener noreferrer" target="_blank"><i class="fab fa-github"></i></a>
                                 <a href="https://www.linkedin.com/in/patryk-jachimowski/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin"></i></a>
                                 <a href="https://dribbble.com/Jachimowski" rel="noopener noreferrer" target="_blank"><i class="fab fa-dribbble"></i></a>
                                 <a href="https://www.instagram.com/pat.wanderlust/?hl=en" rel="noopener noreferrer" target="_blank"><i class="fab fa-instagram"></i></a>
-                            </div>`
+
                         </div>
                     </Cell>
                 </Grid>
