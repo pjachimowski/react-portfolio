a2fea72096b8cd5c90b39a34a815af4885d14c0e
pjachimowski <p.jachimowski89@gmail.com>
2020-10-23 13:11:13 +0200

    footer and new cleaner navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
-import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterDropDownSection, FooterLinkList } from "react-mdl";
+import { Layout, Header, Navigation, Drawer, Content, FooterSection, FooterLinkList, Footer } from "react-mdl";
 import Main from "./components/Main";
 import "./App.css";
 import { Link } from "react-router-dom";
 
-// style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}
-
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="demo-big-content">
         <Layout>
           <Header
-            className="nav-translusent"
-            transparent
+            className="bg-nav header-color"
             title={
               <Link style={{ textDecoration: "none", color: "white" }} to="/">
                 Home
@@ -28,7 +25,13 @@ class App extends Component {
               <Link to="/contact">Contact</Link>
             </Navigation>
           </Header>
-          <Drawer title="Title">
+          <Drawer
+            title={
+              <Link style={{ textDecoration: "none", color: "black" }} to="/">
+                Home
+              </Link>
+            }
+          >
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
@@ -36,43 +39,12 @@ class App extends Component {
               <Link to="/contact">Contact</Link>
             </Navigation>
           </Drawer>
-          <Content />
-          <Main />
-          <Footer className="footer" size="mega">
-            <FooterSection type="middle">
-              <FooterDropDownSection title="Features">
-                <FooterLinkList>
-                  <a href="#">About</a>
-                  <a href="#">Terms</a>
-                  <a href="#">Partners</a>
-                  <a href="#">Updates</a>
-                </FooterLinkList>
-              </FooterDropDownSection>
-              <FooterDropDownSection title="Details">
-                <FooterLinkList>
-                  <a href="#">Specs</a>
-                  <a href="#">Tools</a>
-                  <a href="#">Resources</a>
-                </FooterLinkList>
-              </FooterDropDownSection>
-              <FooterDropDownSection title="Technology">
-                <FooterLinkList>
-                  <a href="#">How it works</a>
-                  <a href="#">Patterns</a>
-                  <a href="#">Usage</a>
-                  <a href="#">Products</a>
-                  <a href="#">Contracts</a>
-                </FooterLinkList>
-              </FooterDropDownSection>
-              <FooterDropDownSection title="FAQ">
-                <FooterLinkList>
-                  <a href="#">Questions</a>
-                  <a href="#">Answers</a>
-                  <a href="#">Contact Us</a>
-                </FooterLinkList>
-              </FooterDropDownSection>
-            </FooterSection>
-            <FooterSection type="bottom" logo="Title">
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+          <Footer className="bg-nav" size="mini">
+            <FooterSection type="left" logo="Title">
               <FooterLinkList>
                 <a href="#">Help</a>
                 <a href="#">Privacy & Terms</a>
